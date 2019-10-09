0e6722e14ad29e00137af3de887f70d9fe7ac8e6
Ruchita Shah <rshah@fredhutch.org>
2019-10-09 12:49:52 -0700

    login logout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { authProvider } from './components/auth/authProvider';
-import AzureAD from 'react-aad-msal';
+import AzureAD, { AuthenticationState } from 'react-aad-msal';
 import LoginButtonLaunch from './components/LoginButtonLaunch';
+import { basicReduxStore } from './components/redux/reduxStore';
+import Home from './components/home.component';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom'
 //import Auth from './components/auth/auth.component';
 
 const App: React.FC = (props: any) => {
@@ -29,15 +33,45 @@ const App: React.FC = (props: any) => {
     //   <a href="#" onClick={handleLogout}>Logout</a>
     //   </div>
     // </AzureAD>
+
+
+      //Try2    
+      // <div className="SampleBox">
+      //   <h2 className="SampleHeader">Automatic Redirect</h2>
+      //   <p>
+      //     This example shows how you can use the AzureAD component to redirect the login screen automatically on
+      //     page load. Click the checkbox below to enable the redirect and refresh your browser window.
+      //   </p>
+      //   <LoginButtonLaunch />
+      // </div>
       
-      <div className="SampleBox">
-        <h2 className="SampleHeader">Automatic Redirect</h2>
-        <p>
-          This example shows how you can use the AzureAD component to redirect the login screen automatically on
-          page load. Click the checkbox below to enable the redirect and refresh your browser window.
-        </p>
-        <LoginButtonLaunch />
-      </div>
+
+      <AzureAD provider={authProvider} reduxStore={basicReduxStore}>
+      {({ login, logout, authenticationState } :  { login: any, logout :any, authenticationState : any}) => {
+        if (authProvider.authenticationState === AuthenticationState.Authenticated) {
+          return (
+            <Router>
+              <React.Fragment>
+              <Switch>            
+                <Route path="/" component={Home} />             
+              </Switch>
+              </React.Fragment>
+            </Router>
+          );
+        } else if (authProvider.authenticationState  === AuthenticationState.Unauthenticated) {
+          return (          
+            <div className="SampleBox">
+              <h2 className="SampleHeader">Automatic Redirect</h2>
+               <p>
+                 This example shows how you can use the AzureAD component to redirect the login screen automatically on
+                 page load. Click the checkbox below to enable the redirect and refresh your browser window.
+               </p>
+               <LoginButtonLaunch />
+             </div> 
+          );
+        }
+      }}
+    </AzureAD>
       
   );
 }
--- src/components/home.component.tsx
+++ src/components/home.component.tsx
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { useDispatch } from 'react-redux';
+import { AuthenticationActions } from 'react-aad-msal';
+import { authProvider } from './auth/authProvider';
 
-class Home extends Component {
-  render() {
+const Home: React.FC = (props: any) => {
+
+  const dispatch = useDispatch();
+  
+
+
+  function logout()
+  {
+    
+    authProvider.logout();
+    dispatch(AuthenticationActions.LogoutSuccess);
+    dispatch({
+      type:AuthenticationActions.LogoutSuccess,
+      payload: logout
+  });
+  }
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2> Home</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={logout} className="Button">
+                Logout
+         </button>
       </div>
     );
-  }
 }
 
 export default Home;
